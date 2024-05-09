510188f45ec42d3b1a9270c075cdbb64a12a24c9
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-05-09 16:58:20 -0500

    agregamos configuracion de fuentes y colores

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'poppins': ['Poppins', 'sans-serif'],
+				'merriweather': ['Merriweather', 'serif'],
+			},
+
+			colors: {
+				'charade': '#2B2D38',
+				'cerise': '#DB2A6B',
+				'silver': '#ABABAB',
+			},
+		},
+
+	},
+	plugins: [],
+}
